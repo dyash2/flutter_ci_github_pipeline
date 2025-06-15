bcac9ec0b4c86031f9dff5f1681e45847fefb9d3
Yash Debnath <ydebnath25@gmail.com>
2025-06-15 13:05:35 +0530

    added github actions pipeline config

--- simple_app/test/widget_test.dart
+++ simple_app/test/widget_test.dart
@@ -11,6 +11,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_app/main.dart';
 
 void main() {
+
+  test('Basic Test',(){
+    expect(2+2, 4);
+  });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
